50c03db610472d9933319532bd443d9efe3ec1b2
Lucas Holtz Wamser <contato@lucaswamser.com>
2018-02-09 00:11:18 -0200

    impl estados

--- smart-rec.js
+++ smart-rec.js
@@ -6,32 +6,40 @@ module.exports = function(RED) {
     function SmartRecNode(config) {
         RED.nodes.createNode(this,config);
 				var node = this;
-				node.status({fill:"yellow",shape:"dot",text:"connecting"});
 				if (config.uid && config.key){
-					node.wss = new WebSocket('wss://s1.smartrec.pressagio.net/websocket/'+config.uid, {
-						perMessageDeflate: false
-					});
-
-					node.wss.addEventListener('error', (err) => {
-						node.error(err)
-						this.status({fill:"red",shape:"dot",text:"error"});
-					});
-
-					node.wss.on('open', function open() {
-						node.status({fill:"yellow",shape:"dot",text:"training"});
-						setTimeout(() => node.status({fill:"green",shape:"dot",text:"ready"}),5000)
-						node.isConnected = true
-						node.wss.on('message', function incoming(data) {
-							var r = JSON.parse(data);
-							if (r.type == "predict"){
-								msg = {payload:r.data}
-								node.send(msg);
-								node.status({fill:"green",shape:"dot",text:"ready"});
-							}
+					node.connect = () =>{
+						node.wss = null;
+					        node.status({fill:"yellow",shape:"dot",text:"connecting"});
+						node.wss = new WebSocket('wss://s1.smartrec.pressagio.net/websocket/'+config.uid, {
+							perMessageDeflate: false
 						});
 
-					});
+						node.wss.addEventListener('error', (err) => {
+							node.error(err)
+							this.status({fill:"red",shape:"dot",text:"error"});
+						});
 
+						node.wss.on('open', function open() {
+							node.isConnected = true
+							node.wss.on('message', function incoming(data) {
+								var r = JSON.parse(data);
+								if (r.type == "predict"){
+									msg = {payload:r.data}
+									node.send(msg);
+								}
+								else if (r.type == "status"){
+									node.status({fill:"green",shape:"dot",text:r.status});
+								}
+							});
+
+						});
+						node.wss.on('close', function close() {
+						  node.status({fill:"red",shape:"dot",text:"disconnected"});
+						  node.isConnected = false;
+						  setTimeout(node.connect,10000);
+						});
+					}
+				   node.connect();
 				}else{
 				 this.status({fill:"red",shape:"dot",text:"not configured"});
 				}
@@ -44,7 +52,6 @@ module.exports = function(RED) {
 					else if (isBuffer(msg.payload)){
 						out.img = msg.payload.toString('base64')
 						node.wss.send(JSON.stringify(out));
-						node.status({fill:"green",shape:"dot",text:"processing"});
 					}else
 						node.error("Type not supported!");
 
